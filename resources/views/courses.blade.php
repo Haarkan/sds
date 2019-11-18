4550e4a2d6f598ee551e71b934a256dd01e6ec41
C3ptV3 <24461272+C3ptV3@users.noreply.github.com>
2019-11-18 17:03:31 +0100

    Ver.1

---
+++ resources/views/courses.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html style="background-color: #152681;width: 1200;margin: 0px;padding: 0px;padding-right: 20px;padding-bottom: 0px;padding-left: 20px;">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Untitled (Backup 1574036777500)</title>
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/Data-Table-1.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/Data-Table.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/Google-Style-Login.css') }}">
+    <link rel="stylesheet" href="{{ URL('https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css">
+</head>
+
+<body class="text-center">
+    <header>
+        <nav class="navbar navbar-light navbar-expand-md sticky-top border-dark border rounded-0" style="padding: 0px;">
+            <div class="container-fluid"><a class="navbar-brand" href="/SDS/public/"><img src="assets/img/EU%20flag-Erasmus+_vect_POS.png" style="width:150px;height:60px;"></a>
+                <div class="collapse navbar-collapse" style="width: 285px;"><a class="d-xl-flex ml-auto" href="#" style="color: rgb(0,0,0);">ERASMUS LEARNING AGREEMENT MANAGER</a></div>
+                <div class="collapse navbar-collapse d-xl-flex justify-content-xl-end" style="width: 0px;height: 34px;margin: 0px;padding: 0px;padding-left: 0px;margin-left: 100px;"><a href="#"><img src="assets/img/shopping-cart.png" style="width: 50px;padding-right: 0px;"></a><a href="#" style="padding-left: 20px;padding-bottom: 0px;"><img src="assets/img/images.jpg" style="width: 50px;"></a></div>
+            </div>
+        </nav>
+    </header>
+    <div></div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="d-xl-flex justify-content-xl-center">LOREM IPSUM</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col"><table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>Course Name</th>
+                <th>Faculty</th>
+                <th>Credit</th>
+                <th>Add</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td><img src="assets/img/shopping-cart.png" alt="" border=3 height=100 width=100></td>
+            </tr>
+            <tr>
+                <td>Garrett Winters</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td><img src="assets/img/shopping-cart.png" alt="" border=3 height=100 width=100></td>
+            </tr>
+            <tr>
+                <td>Ashton Cox</td>
+                <td>Junior Technical Author</td>
+                <td>San Francisco</td>
+                <td><img src="assets/img/shopping-cart.png" alt="" border=3 height=100 width=100></td>
+
+        </tbody>
+    </table></div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,54 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html style="background-color: #152681;width: 1200;margin: 0px;padding: 0px;padding-right: 20px;padding-bottom: 0px;padding-left: 20px;">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Untitled (Backup 1574036777500)</title>
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/Data-Table-1.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/Data-Table.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/Google-Style-Login.css') }}">
+    <link rel="stylesheet" href="{{ URL('https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css">
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<body class="text-center">
+    <header>
+        <nav class="navbar navbar-light navbar-expand-md sticky-top border-dark border rounded-0" style="padding: 0px;">
+            <div class="container-fluid"><a class="navbar-brand" href="#"><img src="assets/img/EU%20flag-Erasmus+_vect_POS.png" style="width:150px;height:60px;"></a>
+                <div class="collapse navbar-collapse" style="width: 285px;"><a class="d-xl-flex ml-auto" href="courses" style="color: rgb(0,0,0);">ERASMUS LEARNING AGREEMENT MANAGER</a></div>
+                <div class="collapse navbar-collapse d-xl-flex justify-content-xl-end" style="width: 0px;height: 34px;margin: 0px;padding: 0px;padding-left: 0px;margin-left: 100px;"><a href="#"><img src="assets/img/shopping-cart.png" style="width: 50px;padding-right: 0px;"></a><a href="#" style="padding-left: 20px;padding-bottom: 0px;"><img src="assets/img/images.jpg" style="width: 50px;"></a></div>
+            </div>
+        </nav>
+    </header>
+    <div></div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="d-xl-flex justify-content-xl-center">LOREM IPSUM</h1>
             </div>
         </div>
-    </body>
+        <div class="row">
+            <div class="col">
+                <p><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lacinia tincidunt velit, vitae dictum diam facilisis commodo. Nunc et tortor mauris. Mauris id ipsum ac dui porta vehicula quis sed arcu. Proin maximus felis ut bibendum
+                    bibendum. Ut condimentum dui nec ante aliquam aliquet. Phasellus finibus quam in tortor gravida, vel luctus lectus vulputate. Sed ut interdum tellus. Curabitur elementum molestie tincidunt. Phasellus sit amet libero imperdiet, aliquam
+                    risus sed, maximus urna. Nulla ultricies purus ex, id sodales ex luctus sed. Aenean sit amet condimentum nulla. Aenean eu elit gravida, gravida magna id, auctor metus. Nam consectetur ante a maximus tincidunt. Donec dapibus euismod
+                    velit, nec commodo felis auctor in. Suspendisse potenti. Nulla finibus scelerisque mi non interdum.Etiam ac leo vitae lectus tincidunt aliquam. Nam mattis eu velit in egestas. Duis euismod suscipit faucibus. In non ultrices magna.
+                    Phasellus urna tortor, tempor eu fermentum in, auctor sit amet libero. Suspendisse rhoncus, metus eu consectetur egestas, risus dui lobortis magna, in egestas lectus massa blandit magna. Quisque at velit ac odio porttitor aliquam.
+                    Nullam non sodales velit, ac venenatis libero. Maecenas lacus justo, condimentum in tempor sit amet, fermentum a dolor. Vivamus sodales tincidunt risus, vel venenatis purus ultrices eget.Fusce vitae mi nibh. Maecenas eleifend nisl
+                    purus, vitae aliquam massa dictum et. Praesent sollicitudin, sapien id finibus interdum, mauris libero scelerisque nibh, et iaculis lorem ex a elit. Aenean maximus sodales tempor. Suspendisse id nisi nec mauris tempus dapibus. Proin
+                    placerat dolor at blandit consequat. Integer facilisis urna erat.Nunc sollicitudin convallis risus. Nullam quis turpis sodales, convallis nisl sed, ullamcorper urna. Quisque erat est, luctus vitae lorem id, molestie feugiat augue.
+                    Quisque id velit viverra, tincidunt sapien ut, dignissim augue. Praesent at sapien ut felis luctus pulvinar vitae nec neque. In eleifend, nisl at facilisis ullamcorper, nunc lacus hendrerit est, sit amet facilisis massa lorem a ligula.
+                    Aenean rutrum rhoncus mi, sit amet pretium risus. Phasellus ornare dui nisl, et congue neque tempor in. Mauris euismod quis sapien nec venenatis. Pellentesque auctor lacus ut turpis hendrerit, eu interdum nisl eleifend. Nunc felis
+                    mauris, egestas quis egestas a, rhoncus sit amet turpis.Nullam rutrum consequat ex eget placerat. Praesent quis aliquam tortor, at malesuada leo. Nulla in facilisis lectus, a consequat enim. Proin laoreet, sem at fermentum finibus,
+                    urna magna finibus orci, euismod accumsan sapien felis et tellus. Nullam bibendum ipsum tellus, a tincidunt nibh vulputate at. Maecenas ut elementum lorem, ac sagittis elit. Integer risus leo, rutrum at est at, dapibus eleifend nisl.
+                    Duis aliquet id urna vel luctus. Donec ut sem elit. Duis dictum vulputate purus sit amet tempor. Fusce elementum, est ut malesuada egestas, arcu arcu condimentum dui, a scelerisque mauris ante in diam. Mauris imperdiet libero et ipsum
+                    luctus, pellentesque posuere ipsum pretium. Fusce non nisi urna.<br><br></p>
+            </div>
+        </div>
+    </div>
+
+</body>
+
 </html>
